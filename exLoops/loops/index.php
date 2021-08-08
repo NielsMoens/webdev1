c3a9290801c938766ebe7518b8b532e5d36a87a4
NielsMoens <nielmoen@student.arteveldehs.be>
2021-08-08 21:48:49 +0200

    all kind of loops

--- exLoops/loops/index.php
+++ exLoops/loops/index.php
@@ -10,14 +10,27 @@
 </head>
 <body>
     <h1>Oef PHP</h1>
-    <h2>Rainbow</h2>
-    <div class="rainbow">
-
-        <?php
+    <div class="card">
+        <h2>Rainbow</h2>
+        <div class="rainbow">
+            <?php
             for ($hue=1 ; $hue<=360 ; $hue++ ){
                 echo '<div class="block" style="background-color: hsl(' . $hue . ', 100%, 50%)"></div>';
             }
-        ?>
+            ?>
+        </div>
     </div>
+    <div class="card">
+        <h2>color array</h2>
+        <div class="array">
+            <?php
+                $color = [1=>'red', 120=>'green', 240=>'blue'];
+                foreach ($color as $key => $value){
+                    echo '<div class="block" style="background-color: '. $value .'">' . $value . '</div>';
+                }
+            ?>
+        </div>
+    </div>
+
 </body>
 </html>
