b760606002410c274379ff5235c129937b2840e3
NielsMoens <nielmoen@student.arteveldehs.be>
2021-08-16 22:22:14 +0200

    :sparkles: :construction: did the basic for the MVC project

---
+++ exMVC/controllers/ProjectController.php
@@ -0,0 +1,24 @@
+<?php
+
+class ProjectController{
+    function index(){
+        echo "index list of projects";
+
+        // db object inladen
+        //sql inladen
+
+        $result=[];
+
+        return $result;
+    }
+    function detail($id = 0){
+        echo "detail page zot eh " . $id ;
+
+        //sql here
+
+        $result=[];
+
+        return $result;
+    }
+
+}
---
+++ exMVC/index.php
@@ -0,0 +1,38 @@
+<?php
+
+require_once 'config.php';
+require_once 'lib/db.php';
+
+
+
+$server = $_SERVER['REQUEST_URI'];
+
+$path = explode('/', $server);
+
+//ucfirst = first letter to capital
+$controller = ucfirst(strtolower($path[3]));
+$method = ( !empty($path[4])) ? strtolower($path[4]) : 'index';
+// path id
+$param = ( !empty($path[5])) ? ($path[5]) : null;
+
+//print_r($method);
+
+$controller_name =$controller . 'Controller';
+$controller_path = 'controllers/' . $controller_name . '.php';
+if (file_exists($controller_path)) {
+    require_once $controller_path;
+
+    //var_dump($controller);
+
+    //dynamic controller an example could be: $controller_name === new ProjectController();
+    $controller_class = new $controller_name();
+
+    if (method_exists($controller_class, $method)) {
+        $controller_class->$method($param);
+    } else {
+        echo 'das ier ni just eh vriendschap';
+    }
+} else{
+    echo 'das ier ni just eh vriendschap';
+}
+?>
